5d7bc01b2c32b9cc079332846de0fb63d295a4c0
卢兴超 <luxingchao@bimwinner.com>
2020-09-24 18:01:42 +0800

    update

--- src/Service/topologyService.js
+++ src/Service/topologyService.js
@@ -1,10 +1,12 @@
 import Axios from '../utils/Service';
 
+const API_ROOT='http://topology.le5le.com'
+
 export async function getListByPage(currentPageIndex) {
-  return await Axios.get(`/api/topologies?pageIndex=${currentPageIndex}&pageCount=8`);
+  return await Axios.get(`${API_ROOT}/api/topologies?pageIndex=${currentPageIndex}&pageCount=8`);
 }
 
 
 export async function getNodeById(id) {
-  return await Axios.get(`/api/topology/${id}`);
+  return await Axios.get(`${API_ROOT}/api/topology/${id}`);
 }
