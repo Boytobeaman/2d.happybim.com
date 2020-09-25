489c101be71c5ab250ab893f293b9bbc26d27b5a
卢兴超 <luxingchao@bimwinner.com>
2020-09-25 10:43:10 +0800

    change api

--- src/Service/topologyService.js
+++ src/Service/topologyService.js
@@ -1,12 +1,12 @@
 import Axios from '../utils/Service';
 
-const API_ROOT='http://topology.le5le.com'
+const API_ROOT='http://api.50d.top/DEMO_APP'
 
 export async function getListByPage(currentPageIndex) {
-  return await Axios.get(`${API_ROOT}/api/topologies?pageIndex=${currentPageIndex}&pageCount=8`);
+  return await Axios.get(`${API_ROOT}/le5le/getListByPage?pageIndex=${currentPageIndex}&pageCount=8`);
 }
 
 
 export async function getNodeById(id) {
-  return await Axios.get(`${API_ROOT}/api/topology/${id}`);
+  return await Axios.get(`${API_ROOT}/le5le/getNodeById/${id}`);
 }
